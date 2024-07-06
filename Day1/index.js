ca23b1fb3692bb6c49f813b9e23f68da8fe3acdf
sawlucky <sawlucky2001@gmail.com>
2024-07-06 20:57:15 +0530

    added files

--- Day1/index.js
+++ Day1/index.js
@@ -1,6 +1,22 @@
 // first creating root element
-const heading = React.createElement("h1", {}, "hello world from reactjs");
+//
+//  <div>
+//  div
+//</div >
+//
+//
+//
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from reactjs"
+);
 //creating root
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement("div", { id: "parent" }, "thi is parent");
+const child = React.createElement("div", { id: "cild" }, "this is child");
+
 //rendering the root element
 root.render(heading);
