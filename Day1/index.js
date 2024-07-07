1c633a4b841ba1b6fcb7444757938bc3d8950dda
sawlucky <sawlucky2001@gmail.com>
2024-07-07 20:23:39 +0530

    updated js file and added more to it

--- Day1/index.js
+++ Day1/index.js
@@ -23,6 +23,36 @@
 const heading = React.createElement("h1", { id: 1 }, "this is heading tag");
 const parent = React.createElement("div", { id: 2 }, "this is parent");
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//  <div id="1">
+//    <div id="2">
+//      <h1>this is me</h1>
+//    </div>
+//  </div>;
+const div = React.createElement(
+  "div",
+  { id: 2 },
+  React.createElement(
+    "div",
+    { id: 2 },
+    React.createElement("h1", { id: "hh" }, "this is heading and i am nested")
+  )
+);
+//<h1>this is me</h1>; if we want to create sibling than we need to place them inside array
+//  <div id="1">
+//    <div id="2">
+//      <h1>this is me</h1>
+//       <h2>this is me</h2>
+//    </div>
+//  </div>;
+const div1 = React.createElement(
+  "div",
+  { id: 2 },
+  React.createElement("div", { id: 2 }, [
+    React.createElement("h1", { id: "hh" }, "this is heading and i am nested"),
+    React.createElement("h2", { id: "hui" }, "this is heading and i am nested"),
+  ])
+);
 root.render(React.createElement(React.Fragment, null, heading, parent));
-root.render([heading, parent]);
+root.render([heading, parent, div, div1]);
 // root.render(parent);
