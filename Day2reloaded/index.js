7b1087af81f2e9c9c69a9c2bdd8c9bdd549883ef
sawlucky <sawlucky2001@gmail.com>
2024-09-12 17:25:28 +0530

    Day2reloadedupdated

---
+++ Day2reloaded/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// browser dont understand  import and export so what nd
+// this is not a normal js file it is module..
+const heading = React.createElement(
+  "h1",
+  { id: "header" },
+  "this is JavaScripts and i want  to master it"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
